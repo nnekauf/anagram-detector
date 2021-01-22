563b675aadbe909192716d3089f9cfcd54e3697a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-21 21:40:46 -0500

    commit tree 6ef659f506f6de97a6fd9defd665d72caea0536e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        
+       #matched_words= []
+
+       array.select{|anagram| anagram if anagram.split("").sort == @word.split("").sort } 
+    end
+
+end
